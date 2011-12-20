f6a18fedc14f087a3326ae786860c57a04caef0a
James Tharpe <jimmy.tharpe@gmail.com>
2011-12-20 17:35:01 -0500

    Added operators to make teting & other functions easier.

--- src/JamesGames.PlayingCards/PlayingCard.cs
+++ src/JamesGames.PlayingCards/PlayingCard.cs
@@ -10,8 +10,9 @@ namespace JamesGames.PlayingCards
     /// </summary>
     public class PlayingCard
     {
+        #region Constructors
         /// <summary>
-        /// Constructor.
+        /// Initializes a new instance of PlayingCard.
         /// </summary>
         /// <param name="suit">Specifies the card suit (hearts, spades, etc).</param>
         /// <param name="face">Specifies the card face (10, Jack, etc).</param>
@@ -22,10 +23,11 @@ namespace JamesGames.PlayingCards
         }
 
         /// <summary>
-        /// Default constructor.
+        /// Initializes a new instance of PlayingCard.
         /// </summary>
         public PlayingCard()
             : this(PlayingCardSuit.Spades, PlayingCardFace.Ace) { }
+        #endregion Constructors
 
         /// <summary>
         /// Gets and sets a value indicating the playing card suit (hearts, clubs...).
@@ -36,5 +38,30 @@ namespace JamesGames.PlayingCards
         /// Gets and sets a value indicating the playing card face (ace, two, three...).
         /// </summary>
         public PlayingCardFace Face { get; set; }
+
+        #region Operators
+        public override bool Equals(object obj)
+        {
+            return (this == (PlayingCard)obj);
+        }
+
+        public static bool operator ==(PlayingCard card1, PlayingCard card2)
+        {
+            if (object.ReferenceEquals(card1, card2))
+                return true; // For two nulls
+            if (object.ReferenceEquals(card1, null) || object.ReferenceEquals(card2, null))
+                return false; // For one nulls, one not null
+            return ((card1.Face == card2.Face) && (card1.Suit == card2.Suit));
+        }
+
+        public static bool operator !=(PlayingCard card1, PlayingCard card2)
+        {
+            if (object.ReferenceEquals(card1, card2))
+                return false; // For nulls
+            if (object.ReferenceEquals(card1, null) || object.ReferenceEquals(card2, null))
+                return true; // For one nulls, one not null
+            return ((card1.Face != card2.Face) || (card1.Suit != card2.Suit));
+        }
+        #endregion Operators
     }
 }
