f7535d4f9a4e0650f0f70cdbde3f9ad059cb82f3
James Tharpe <jimmy.tharpe@gmail.com>
2012-01-02 11:13:38 -0500

    Documentation.

--- src/JamesGames.War/WarCardComparer.cs
+++ src/JamesGames.War/WarCardComparer.cs
@@ -11,10 +11,18 @@ namespace JamesGames.War
     /// </summary>
     class WarCardComparer : Comparer<PlayingCard>
     {
+        /// <summary>
+        /// Performs a comparison of two playing cards and returns a value indicating whether one 
+        /// card is less than, equal to, or greater than the other.
+        /// </summary>
+        /// <param name="x">The first card to compare.</param>
+        /// <param name="y">The second card to compare.</param>
         public override int Compare(PlayingCard x, PlayingCard y)
         {
             int xCardValue = (x.Face == PlayingCardFace.Ace) ? 14 : (int)x.Face;
             int yCardValue = (y.Face == PlayingCardFace.Ace) ? 14 : (int)y.Face;
+
+            // ??? return xCardValue - yCardValue;
 
             if (xCardValue > yCardValue)
                 return 1;
