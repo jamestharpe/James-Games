b2b65d5a932d03076ed2843332bd53bbf20c337a
James Tharpe <jimmy.tharpe@gmail.com>
2012-01-02 10:05:52 -0500

    Added a console app that outputs debug info for easy testing.

---
+++ src/JamesGames.War.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rolcore;
+using Rolcore.Diagnostics;
+using System.Diagnostics;
+
+namespace JamesGames.War.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WarGame game = new WarGame(
+                new WarPlayer() { Name = "Player 1" },
+                new WarPlayer() { Name = "Player 2" });
+
+            Debug.Listeners.Add(new ConsoleTraceListener());
+            try
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    //while (game.GameState != WarGameState.GameOver)
+                    {
+                        game.AdvancePlay();
+                        Debug.WriteLine(string.Empty);
+                    }    
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                Debug.WriteLine("Done");
+                Console.ReadLine();    
+            }
+            
+        }
+    }
+}
